549e293977b7781057723cfeecb425c2feada7a8
Charles <galc201093@gmail.com>
2017-11-23 13:59:27 +0100

    Adding Zoom to the map

--- src/analysis.js
+++ src/analysis.js
@@ -2,10 +2,19 @@
 const width = window.innerWidth;
 const height = window.innerHeight;
 
+var active = d3.select(null);
+
 // D3 Projection
 const projection = d3.geoEquirectangular()
     .scale(width / 2 / Math.PI)
-	.translate([width/2, height / 2]);
+    .translate([width/2, height / 2]);
+    
+var zoom = d3.zoom()
+    // no longer in d3 v4 - zoom initialises with zoomIdentity, so it's already at origin
+    // .translate([0, 0]) 
+    // .scale(1) 
+    .scaleExtent([1, 8])
+    .on("zoom", zoomed);
 
 // path generator to convert JSON to SVG paths
 const path = d3.geoPath()
@@ -24,9 +33,21 @@ const svg = d3.select("body")
     .append("svg")
     .attr("id", "map")
     .attr("viewBox", "0 0 " + width + " " + height )
-    .attr("preserveAspectRatio", "xMidYMid meet");
+    .attr("preserveAspectRatio", "xMidYMid meet")
+    .on("click", stopped, true);
+
+/*
+svg.append("rect")
+    .attr("class", "background")
+    .attr("width", width)
+    .attr("height", height)
+    .on("click", reset);
+*/
 
 
+svg.call(zoom);
+
+var g = svg.append("g");
 
 function updateTemperature(countries, data) {
     //TODO try avoid the double loop (sorting once for all)
@@ -52,8 +73,10 @@ function updateTemperature(countries, data) {
         }
     }
 }
+
+//TODO Better speparate code (createMap() and colorMap())
 function renderCountryColor(countries, color) {
-    svg.selectAll("path")
+    g.selectAll("path")
     .data(countries.features)
     .enter()
     .append("path")
@@ -62,12 +85,13 @@ function renderCountryColor(countries, color) {
     .style("fill", (d) => {
             const temperature = d.properties.temperature;
             return temperature ? color(temperature) : "white";//color(temperature);
-            });
+            })
+    .on("click", clicked);
 }
 
 function updateCountryColor(countries, color) {
 
-    svg.selectAll(".countries")
+    g.selectAll(".countries")
     .data(countries.features)
     .attr("id", (d) => d.id)
     .style("fill", (d) => {
@@ -118,3 +142,48 @@ d3.csv("data/output.csv", function(data) {
 		
 	});
 });
+
+
+
+//ZOOM
+
+function clicked(d) {
+    if (active.node() === this) return reset();
+    active.classed("active", false);
+    active = d3.select(this).classed("active", true);
+  
+    var bounds = path.bounds(d),
+        dx = bounds[1][0] - bounds[0][0],
+        dy = bounds[1][1] - bounds[0][1],
+        x = (bounds[0][0] + bounds[1][0]) / 2,
+        y = (bounds[0][1] + bounds[1][1]) / 2,
+        scale = Math.max(1, Math.min(8, 0.9 / Math.max(dx / width, dy / height))),
+        translate = [width / 2 - scale * x, height / 2 - scale * y];
+  
+    svg.transition()
+        .duration(750)
+        // .call(zoom.translate(translate).scale(scale).event); // not in d3 v4
+        .call( zoom.transform, d3.zoomIdentity.translate(translate[0],translate[1]).scale(scale) ); // updated for d3 v4
+}
+  
+function reset() {
+    active.classed("active", false);
+    active = d3.select(null);
+
+    svg.transition()
+        .duration(750)
+        // .call( zoom.transform, d3.zoomIdentity.translate(0, 0).scale(1) ); // not in d3 v4
+        .call( zoom.transform, d3.zoomIdentity ); // updated for d3 v4
+}
+
+function zoomed() {
+    g.style("stroke-width", 1.5 / d3.event.transform.k + "px");
+    // g.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")"); // not in d3 v4
+    g.attr("transform", d3.event.transform); // updated for d3 v4
+  }
+
+// If the drag behavior prevents the default click,
+// also stop propagation so we don’t click-to-zoom.
+function stopped() {
+    if (d3.event.defaultPrevented) d3.event.stopPropagation();
+  }
