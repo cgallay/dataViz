bfc72fc4d790600c2b99031f95767e399fc3a655
Charles Gallay <charles.gallay@epfl.ch>
2017-12-22 12:09:45 +0100

    Charles: adding selection to button

--- src/buttonManager.js
+++ src/buttonManager.js
@@ -5,11 +5,22 @@ export class ButtonManger{
     constructor(mapRef) {
         this.mapRef = mapRef;
         this.tempButton = $('#tempButton');
+        this.co2Button = $('#CO2Button');
+
         this.tempButton.click((d) => {
-            mapRef.setValueType('TEMPERATURE');
+            this.mapRef.setValueType('TEMPERATURE');
+            if (!this.tempButton.hasClass('selected')) {
+                this.co2Button.removeClass('selected');
+                this.tempButton.addClass('selected');
+            }
         });
-        $('#CO2Button').click((d) => {
-            mapRef.setValueType('CO2');
+
+        this.co2Button.click((d) => {
+            this.mapRef.setValueType('CO2');
+            if (!this.co2Button.hasClass('selected')) {
+                this.tempButton.removeClass('selected');
+                this.co2Button.addClass('selected');
+            }
         });
     }
 
